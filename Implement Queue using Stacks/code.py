420d54833a7850cc7eac38213a9b111267b1f692
KatiaHushchuk <149365916+KatiaHushchuk@users.noreply.github.com>
2024-04-15 19:41:04 +0300

    Update code.py

--- Implement Queue using Stacks/code.py
+++ Implement Queue using Stacks/code.py
@@ -1,56 +1,57 @@
 class Node:
-    def __init__(self, data, prev=None, next=None):
+    '''class for nodes'''
+    def __init__(self, data):
         self.data = data
-        self.prev = prev
-        self.next = next
+        self.next = None
 
 class Stack:
+    '''class for stack'''
     def __init__(self):
-        self.top = None  
+        self.head = None
 
     def is_empty(self):
-        return self.top is None
+        '''returns true if stack is empty and false otherwise'''
+        return not self.head
 
     def push(self, data):
+        '''pushes data to the top of the stack'''
         new_node = Node(data)
-        if self.is_empty():
-            self.top = new_node
-        else:
-            new_node.next = self.top
-            self.top = new_node
+        new_node.next = self.head
+        self.head = new_node
 
     def pop(self):
+        '''pops the top element of the stack'''
         if self.is_empty():
-            raise ValueError("Stack is empty")
-        data = self.top.data
-        self.top = self.top.next
-        return data
+            return None
+        node = self.head.data
+        self.head = self.head.next
+        return node
 
     def peek(self):
+        '''returns the top element of the stack'''
         if self.is_empty():
-            raise ValueError("Stack is empty")
-        return self.top.data
+            return None
+        return self.head.data
 
 class MyQueue:
     def __init__(self):
-        self.in_stack = Stack()
-        self.out_stack = Stack()
-
+        self.queue = Stack()
+        
     def push(self, x: int) -> None:
-        self.in_stack.push(x)
+        reverse_new_stack = Stack()
+        while not self.queue.is_empty():
+            el = self.queue.pop()
+            reverse_new_stack.push(el)
+        self.queue.push(x)
+        while not reverse_new_stack.is_empty():
+            el = reverse_new_stack.pop()
+            self.queue.push(el)
 
     def pop(self) -> int:
-        self._move_to_out_stack()
-        return self.out_stack.pop()
-
+        return self.queue.pop()
+        
     def peek(self) -> int:
-        self._move_to_out_stack()
-        return self.out_stack.peek()
+        return self.queue.peek()
 
     def empty(self) -> bool:
-        return self.in_stack.is_empty() and self.out_stack.is_empty()
-
-    def _move_to_out_stack(self):
-        if self.out_stack.is_empty():
-            while not self.in_stack.is_empty():
-                self.out_stack.push(self.in_stack.pop()) 
+        return self.queue.is_empty()
